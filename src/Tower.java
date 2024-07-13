8e1971f12ad8b18f743a16b3c9e452f0871f9ee2
Eltun Aslanov <aslanoveltun28@gmail.com>
2024-07-13 11:05:16 +0400

    try fix error

---
+++ src/Tower.java
@@ -0,0 +1,38 @@
+public class Tower {
+    private static int healthPoint=100;
+    static Enemy[][] enemies=new Enemy[15][5];
+    static TowerDefender[][] defenders=new TowerDefender[3][5];
+    public static void displayTower() {
+        System.out.print("""
+                   %s     %s     %s     %s     %s
+                   %s     %s     %s     %s     %s
+                   %s     %s     %s     %s     %s
+                   %s     %s     %s     %s     %s
+                   %s     %s     %s     %s     %s
+                   %s     %s     %s     %s     %s
+                   %s     %s     %s     %s     %s
+                   %s     %s     %s     %s     %s
+                   %s     %s     %s     %s     %s
+                   %s     %s     %s     %s     %s
+                   %s     %s     %s     %s     %s
+                   %s     %s     %s     %s     %s
+                   %s     %s     %s     %s     %s
+                   %s     %s     %s     %s     %s
+                   %s     %s     %s     %s     %s
+                   %s     %s     %s     %s     %s
+                _______________________________
+                |     |     |     |     |     |
+                |  %s  |  %s  |  %s  |  %s  |  %s  |
+                |_____|_____|_____|_____|_____|
+                |     |     |     |     |     |
+                |  %s  |  %s  |  %s  |  %s  |  %s  |
+                |_____|_____|_____|_____|_____|
+                |     |     |     |     |     |
+                |  %s  |  %s  |  %s  |  %s  |  %s  |
+                |_____|_____|_____|_____|_____|
+                """);
+        int length=(healthPoint+"").length();
+        System.out.println(" ".repeat(Math.max(0, 15-length/2))+healthPoint
+                           +" ".repeat(Math.max(0, 15-length/2+(length%2==0?1:0)))+"\n\n");
+    }
+}
